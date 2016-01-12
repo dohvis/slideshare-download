d9363eeb4760c7b3884b078dc4c90d722eb5fe3b
Kim Do Hyun <nero.union12@gmail.com>
2016-01-12 17:22:32 +0900

    Add basic models in all-in-one.py with flask-migrate and flask-ext. Close #7

--- all-in-one.py
+++ all-in-one.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template, request as req, send_from_directory
+from flask import Flask, render_template, request as req, send_from_directory, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager, Server
+from flask.ext.migrate import Migrate, MigrateCommand
 from celery import Celery
 from os import path, walk, system, makedirs
 from os.path import join, abspath
 
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
 
 MEDIA = '/tmp/slideshare-vpn/'
 TEMPLATE_DIR = path.join(path.dirname(path.abspath(__file__)), 'templates')
@@ -15,8 +23,73 @@ def create_app():
     return app
 
 app = create_app()
+db = SQLAlchemy(app)
 print(app.config['CELERY_BROKER_URL'])
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+
+
+users_slides = db.Table('users_slides',
+    db.Column("user_id", db.Integer, db.ForeignKey("user.idx")),
+    db.Column("slide_id", db.Integer, db.ForeignKey("slide.idx"))
+    )
+
+
+class User(db.Model):
+    from datetime import datetime
+    __tablename__ = "user"
+    idx = db.Column(db.Integer(), primary_key=True)
+    email = db.Column(db.String(64))
+    pw = db.Column(db.String(32))
+    name = db.Column(db.String(32))
+    fb_id = db.Column(db.String(30), unique=True)
+    is_active = db.Column(db.BOOLEAN, nullable=False, default=True)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    slides = db.relationship('Slide', secondary=users_slides, backref='users')
+
+    def __init__(self, email, pw, name,  fb_id=None):
+        self.email = email
+        self.pw = pw
+        self.name = name
+        self.fb_id = fb_id
+
+
+class Slide(db.Model):
+    __tablename__ = "slide"
+    idx = db.Column(db.Integer(), primary_key=True)
+    slideshare_url = db.Column(db.String(255))
+    thumbnail = db.Column(db.String(255))
+    pdf_path = db.Column(db.String)
+    views = db.Column(db.Integer, default=0)
+
+    def __init__(self, slideshare_url, thumbnail, pdf_path):
+        self.slideshare_url = slideshare_url
+        self.thumbnail = thumbnail
+        self.pdf_path = pdf_path
+
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+manager.add_command('run', Server(host='0.0.0.0', port=5000))
+
+@manager.command
+def list_routes():
+    import urllib
+    output = []
+    for rule in app.url_map.iter_rules():
+
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
+
+        methods = ','.join(rule.methods)
+        url = url_for(rule.endpoint, **options)
+        line = urllib.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
+        output.append(line)
+
+    for line in sorted(output):
+        print(line)
 
 
 @app.route('/static/<path:filename>')
@@ -85,4 +158,4 @@ def result():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    manager.run()
